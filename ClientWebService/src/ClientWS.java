df43e4336472fd71326a559cdc0b46a0b169b9b8
Hasna Bouisskrane <61788817+hasnabouisskrane@users.noreply.github.com>
2022-04-16 01:51:10 +0200

    Add files via upload

---
+++ ClientWebService/src/ClientWS.java
@@ -0,0 +1,26 @@
+import proxy.BanqueService;
+import proxy.BanqueWS;
+import proxy.Compte;
+
+import java.util.List;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService stubWS=new BanqueWS().getBanqueServicePort();
+        System.out.println(stubWS.conversionEuroToDH(800));
+        Compte cp=stubWS.getCompte(2L);
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+        List<Compte> cptes=stubWS.listComptes();
+        cptes.forEach( c -> {
+            System.out.println("------------");
+            System.out.println(c.getCode());
+            System.out.println(c.getSolde());
+
+
+
+        });
+
+
+    }
+}
